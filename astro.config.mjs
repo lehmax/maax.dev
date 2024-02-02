361e8a59dffbbea751ac42f3cf3d0b18eeb32621
Maxime Leherquer <45520008+lehmax@users.noreply.github.com>
2024-02-02 03:48:29 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@ import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 
-import vercel from "@astrojs/vercel/serverless"
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://maax.dev",
   integrations: [react(), tailwind()],
   output: "static",
-  adapter: vercel(),
 })

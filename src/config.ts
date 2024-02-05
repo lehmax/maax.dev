c479d7302663d3511b27f86458db608b8e1d108d
Maxime Leherquer <45520008+lehmax@users.noreply.github.com>
2024-02-05 23:07:07 +0100

    WIP Blog

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-  site_name: "Maxime Leherquer - Frontend Developer",
-  title: "Maxime Leherquer - Frontend Developer",
-  description: "Hi I'm Maxime Leherquer, a Frontend Developer based in Caen, France.",
-  locale: "en",
-}

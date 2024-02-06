d81b3703331054d22f3b20a0e498a1673c37b015
Maxime Leherquer <45520008+lehmax@users.noreply.github.com>
2024-02-06 20:03:53 +0100

    WIP Blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/serverless"
+import compress from "astro-compress"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://maax.dev",
-  integrations: [react(), tailwind()],
-  output: "static",
+  integrations: [tailwind(), sitemap(), compress()],
+  output: "server",
+  adapter: vercel(),
 })
